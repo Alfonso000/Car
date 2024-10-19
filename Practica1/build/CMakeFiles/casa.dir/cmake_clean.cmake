5ce8248d69bfa4157fba8fc7bc4447a22a247f12
Alfonso000 <alfonsosanchis0@gmail.com>
2024-10-19 12:16:08 +0200

    cambios iniciales

---
+++ Practica1/build/CMakeFiles/casa.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/casa.dir/src/Ejercicio1.c.o"
+  "CMakeFiles/casa.dir/src/Ejercicio1.c.o.d"
+  "casa"
+  "casa.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/casa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Practica1/build/CMakeFiles/casa.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for casa.
+# This may be replaced when dependencies are built.
